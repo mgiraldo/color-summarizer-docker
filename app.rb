35eb9b288010347a638ee23309c0608293bd3eb1
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-05-02 17:30:44 -0400

    :space_invader: with param

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra'
 require "sinatra/json"
 require 'open3'
+require 'open-uri'
 
 get '/:format?' do
-  # url = params['url']
+  temp_file = "temp.jpg"
+  url = params['url']
+  open(temp_file, "wb") do |file|
+    file << open(url).read
+  end
   output = ""
-  Open3.popen3("perl -X /usr/src/app/bin/colorsummarizer img/ferns-100.jpg -width 50 -json
+  Open3.popen3("perl -X /usr/src/app/bin/colorsummarizer /usr/src/app/#{temp_file} -json
 ") {|i,o,e,t|
     output = output + o.read.chomp
   }
+  File.delete(temp_file)
   json JSON.parse(output)
 end
 
