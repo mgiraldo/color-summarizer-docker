7f67fb0245c629bf80e63a47cf132264814a0a23
Mauricio Giraldo <hello@mauriciogiraldo.com>
2020-03-15 22:51:46 +1100

    :space_invader: fixed conflict in args

--- summarize_csv.py
+++ summarize_csv.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", default="/usr/src/app/files/files-urls.csv", type=str, help="path to CSV file (default: /usr/src/app/files/files-urls.csv)")
 parser.add_argument("-o", "--origin_folder", default="/usr/src/app/files", type=str, help="Folder where images are located (default: /usr/src/app/files)")
 parser.add_argument("-d", "--destination", default="/usr/src/app/files/colors_output", type=str, help="Folder where to save results (default: /usr/src/app/files/colors_output)")
-parser.add_argument('-o', '--overwrite', action='store_false', help='Overwrite existing json if it exists')
+parser.add_argument('--overwrite', action='store_false', help='Overwrite existing json if it exists')
 parser.add_argument("-p", "--processes", default=None, type=int, help="How many processes (default: None which lets Python decide)")
 
 args = parser.parse_args()
