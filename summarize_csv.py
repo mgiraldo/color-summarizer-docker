16fe7351fd57075258c2a4b25591e14ed54a8bb5
Mauricio Giraldo <hello@mauriciogiraldo.com>
2020-03-15 22:24:49 +1100

    :space_invader: updated to use filename instead of file_key /  added help info

--- summarize_csv.py
+++ summarize_csv.py
@@ -10,9 +10,9 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-f", "--file", default="/usr/src/app/files/files-urls.csv", type=str, help="path to CSV file")
-parser.add_argument("-o", "--origin_folder", default="/usr/src/app/files", type=str, help="Folder where images are located")
-parser.add_argument("-d", "--destination", default="/usr/src/app/files/colors_output", type=str, help="Folder where to save results")
+parser.add_argument("-f", "--file", default="/usr/src/app/files/files-urls.csv", type=str, help="path to CSV file (default: /usr/src/app/files/files-urls.csv)")
+parser.add_argument("-o", "--origin_folder", default="/usr/src/app/files", type=str, help="Folder where images are located (default: /usr/src/app/files)")
+parser.add_argument("-d", "--destination", default="/usr/src/app/files/colors_output", type=str, help="Folder where to save results (default: /usr/src/app/files/colors_output)")
 parser.add_argument("-p", "--processes", default=None, type=int, help="how many processes (default: None which lets Python decide)")
 
 args = parser.parse_args()
@@ -39,7 +39,7 @@ if (os.path.exists(destination) == False):
 
 url_df = pd.read_csv(file, index_col="access_pid")
 
-count = len(url_df.file_key)
+count = len(url_df.filename)
 
 skipped = []
 
@@ -47,8 +47,8 @@ print("Summarizing %s files with %s processes" % (count, (multiprocessing.cpu_co
 
 def summarize_row(access_pid, row):
   try:
-    file_key = row["file_key"]
-    image = "%s/%s" % (origin_folder, file_key)
+    filename = row["filename"]
+    image = "%s/%s/%s" % (origin_folder, filename[0:4], filename)
     json = "%s/%s.json" % (destination, access_pid)
     if (Path(json).exists() == False):
       subprocess.run(["python", "summarize_file.py", "-s", image, json])
